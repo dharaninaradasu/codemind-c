97acdce7618bc301b1df912736882514d9d75425
dharaninaradasu <147400712+dharaninaradasu@users.noreply.github.com>
2023-10-14 06:07:17 +0530

    #include<stdio.h> int main() {     char x,a,e,i,o,u,A,E,I,O,U;     scanf("%c",&x);     if (x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')     {     printf("VOWEL");     }  else  {      printf("CONSONANT");  } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char x,a,e,i,o,u,A,E,I,O,U;
+    scanf("%c",&x);
+    if (x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+    {
+    printf("VOWEL");
+    }
+ else 
+ {
+     printf("CONSONANT");
+ }
+}
