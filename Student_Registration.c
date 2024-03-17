bbd5ec40f293bfb719e6f2ed496c4c97c977ea12
dharaninaradasu <147400712+dharaninaradasu@users.noreply.github.com>
2024-03-17 09:37:12 +0530

    #include<stdio.h> int main() {     int t,n,m,k;     scanf("%d",&t); while(t--){     int n,m,k;     scanf("%d %d %d",&n,&m,&k);     if(n<=m-k){         printf("YES ");     }         else {             printf("NO ");         }     } }

---
+++ Student_Registration.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int t,n,m,k;
+    scanf("%d",&t);
+while(t--){
+    int n,m,k;
+    scanf("%d %d %d",&n,&m,&k);
+    if(n<=m-k){
+        printf("YES
+");
+    }
+        else {
+            printf("NO
+");
+        }
+    }
+}
